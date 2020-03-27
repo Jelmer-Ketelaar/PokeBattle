40d0733875d3f399923e0f680e9b8f1045463b7e
Jelmer Ketelaar <99053633@mydavinci.nl>
2020-03-27 12:10:47 +0100

    Added charmeleon

---
+++ Charmeleon.php
@@ -0,0 +1,19 @@
+<?php
+
+class Charmeleon extends Pokemon
+{
+        public function __construct($Name)
+        {
+            $EnergyType = new EnergyType("Fire");
+            $Hitpoints = 60;
+            $Health = $Hitpoints;
+            $Attacks = [
+                new Attacks("Head Butt", 10),
+                new Attacks("Flare", 30)
+            ];
+            $Weakness = new Weakness("Water", 2);
+            $Resistance = new Resistance("Lightning", 10);
+
+            parent::__construct($Name, $EnergyType, $Hitpoints, $Health, $Attacks, $Weakness, $Resistance);
+        }
+}
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require "Pokemon.php";
 require "Pikachu.php";
+require "Charmeleon.php";
 require "EnergyType.php";
 require "Attacks.php";
 require "Weakness.php";
@@ -9,3 +10,6 @@ require "Resistance.php";
 
 $Pikachu = new Pikachu('Pikachu');
 print_r('<pre>' . $Pikachu . '</pre>');
+
+$Charmeleon = new Charmeleon('Charmeleon');
+print_r('<pre>' . $Charmeleon . '</pre>');  
