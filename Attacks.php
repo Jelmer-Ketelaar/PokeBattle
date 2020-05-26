8920ed8a8b6240235500cb83eacf819096059260
Jelmer Ketelaar <jelmer@ketelaar.me>
2020-05-26 15:11:44 +0200

    [TASK] Added the attacks

--- Attacks.php
+++ Attacks.php
@@ -1,5 +1,6 @@
 <?php
 
+//Class attacks is created with a Name and Hitpoints
 class Attacks
 {
     public $Name;
@@ -10,6 +11,28 @@ class Attacks
         $this->Name = $Name;
         $this->Hitpoints = $Hitpoints;
     }
+
+    //If the weakness enerytpe is equal to the energy type of the attacking pokemon, the damage x the multiplier is done.
+    public function multiplyDamage($multiplier)
+    {
+        $this->Hitpoints = $this->Hitpoints * $multiplier;
+    }
+    //If the resistance energy type is equal to the energy type of the attacking pokemon, the damage - resistance is done.
+    public function reduceDamage($resistance)
+    {
+        $this->Hitpoints = $this->Hitpoints - $resistance;
+    }
+
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+    public function getDamage()
+    {
+        return $this->Hitpoints;
+    }
+
 }
 
 
