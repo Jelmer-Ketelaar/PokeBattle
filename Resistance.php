5ce8ba8fe8e4c252aab740e9795196dfc83e088a
Jelmer Ketelaar <jelmer@ketelaar.me>
2021-03-02 17:18:23 +0100

    Added public function for resistanceType

--- Resistance.php
+++ Resistance.php
@@ -1,18 +1,24 @@
 <?php
 
-    class Resistance {
-        public $EnergyType;
-        public $Value;
+class Resistance
+{
+    public $EnergyType;
+    public $reducedDamage;
 
-        public function __construct($EnergyType, $Value)
-        {
-            $this->EnergyType = $EnergyType;
-            $this->Value = $Value;
-        }
-
-        public function getResistanceType()
-        {
-            return $this->EnergyType;
-        }
+    public function __construct($EnergyType, $Value)
+    {
+        $this->EnergyType = $EnergyType;
+        $this->reducedDamage = $Value;
     }
+
+    public function getResistanceType()
+    {
+        return $this->EnergyType;
+    }
+
+    public function getReducedDamage()
+    {
+        return $this->reducedDamage;
+    }
+}
 
