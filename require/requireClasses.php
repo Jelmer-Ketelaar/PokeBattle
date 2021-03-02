a0bae84f5318439023f9076705e6c504a81d28a1
Jelmer Ketelaar <jelmer@ketelaar.me>
2021-03-02 17:11:14 +0100

    Create requireClasses.php

---
+++ require/requireClasses.php
@@ -0,0 +1,10 @@
+<?php
+//Laad alle bestanden in
+require "Pokemon.php";
+require "Pikachu.php";
+require "Charmeleon.php";
+require "EnergyType.php";
+require "Attacks.php";
+require "Weakness.php";
+require "Resistance.php";
+require "Statistics.php";
